78dadb9e08a5c9d415f23fd32cfa9ec84f297d06
Felipe <lipepbarroso5@gmail.com>
2024-10-29 23:33:33 +0000

    Inicio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,44 +2,37 @@
 
     int main(){
         // Declaração de variáveis
-    int pontos_t;
-    char nome[50];
-    char codigo[50];
-    char populacao[50];
-    char area[50];
-    char PIB[50];
+        char nome[30];
+        char codigo[3];
+        int pontos_turisticos;
+        int populacao, area , PIB;
+        float densidade_populacional, PIB_percapta;
 
-    // Saída de dados com printf e entrada de dados com scanf
-    printf("Qual o codigo da cidade?\n");
-    scanf("%3s",codigo);
 
-    printf("Qual o nome da cidade?\n");
-    scanf(" %50[^\n]",nome);            
+        printf("***Bem vindo ao Super Trunfo***\n");
 
-    printf("Qual a população da cidade?\n");
-    scanf("%s",&populacao);
+        printf("Qual o código de sua carta?\n");
+        scanf("%3s",&codigo);
 
-    printf("Qual da área da cidade?\n");
-    scanf("%s",&area);
+        printf("Qual Nome da cidade?\n");
+        scanf("%s",&nome);
 
-    printf("Qual o PIB da cidade?\n");
-    scanf("%s",&PIB);
+        printf("Quantos pontos turisticos?\n");
+        scanf("%d",&pontos_turisticos);
 
-    printf("Quantos pontos turísticos?\n");
-    scanf("%d",&pontos_t);
+        printf("Qual a área da sua cidade?\n");
+        scanf("%d",&area);
 
-    
-    // Detalhes das cartas de Super Trunfo
-    printf("Codigo da cidade: %s\n",codigo);
-    printf("Nome da cidade: %s\n",nome);
-    printf("População: %s\n",populacao);
-    printf("Área: %s\n",area);
-    printf("PIB: %s\n",PIB);
-    printf("Pontos turísticos: %d\n",pontos_t);
+        printf("Qual a população da sua cidade?\n");
+        scanf("%d",&populacao);
+
+        printf("Qual o PIB da sua cidade?\n");
+        scanf("%d",&codigo);
+
+       
 
 
 
-   
 
 
  return 0;
